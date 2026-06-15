9eb407bf99c16f2148de2dcdb36d0893fe3c9ff8
Nicolás Villegas Castillo <navillegas@miuandes.cl>
2026-06-15 16:38:42 -0400

    Upgrade pkg for 16 KB size

--- rtl/croc_pkg.sv
+++ rtl/croc_pkg.sv
@@ -52,7 +52,7 @@ package croc_pkg;
   /// Number of SRAM banks, each bank has its own OBI port (accessible in parallel)
   localparam int unsigned NumSramBanks      = 32'd2;
   /// Number of 32-bit words per SRAM bank, determines the depth of each SRAM bank
-  localparam int unsigned SramBankNumWords  = 512;
+  localparam int unsigned SramBankNumWords  = 2048;
 
 
   //////////////////////
@@ -88,8 +88,8 @@ package croc_pkg;
   localparam addr_map_rule_t [3:0] CrocAddrMap = '{
     '{ idx: XbarPeriph,  start_addr: 32'h0000_0000, end_addr: 32'h1000_0000 },
     '{ idx: XbarUser,    start_addr: 32'h2000_0000, end_addr: 32'h8000_0000 },
-    '{ idx: XbarBank0,   start_addr: 32'h1000_0000, end_addr: 32'h1000_0800 },
-    '{ idx: XbarBank0+1, start_addr: 32'h1000_0800, end_addr: 32'h1000_1000 }
+    '{ idx: XbarBank0,   start_addr: 32'h1000_0000, end_addr: 32'h1000_2000 },
+    '{ idx: XbarBank0+1, start_addr: 32'h1000_2000, end_addr: 32'h1000_4000 }
   };
 
   // +1 for additional OBI error
